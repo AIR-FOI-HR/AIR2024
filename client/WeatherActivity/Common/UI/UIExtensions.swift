d82bbeaee04057dbca7071c7dde90e3389b65b0f
Kevin Bogdan <kevinwebd31@gmail.com>
2020-11-27 13:06:49 +0100

    Adding xcworkspace

--- client/WeatherActivity/Common/UI/UIExtensions.swift
+++ client/WeatherActivity/Common/UI/UIExtensions.swift
@@ -8,7 +8,7 @@
 import UIKit
     
 extension UITextField: UITextFieldDelegate {
-    public func textFieldDidBeginEditing(_ textFieldEditing: UITextField) {
+    public func updateTextAppearanceOnFieldDidBeginEditing(_ textFieldEditing: UITextField) {
         textFieldEditing.layer.borderColor = UIColor(red:115/255, green:204/255, blue:255/255, alpha: 1).cgColor
         textFieldEditing.layer.borderWidth = 1.0
         textFieldEditing.layer.cornerRadius = 5.0
@@ -16,7 +16,7 @@ extension UITextField: UITextFieldDelegate {
         textFieldEditing.tintColor = .some(UIColor(red:30/255, green:53/255, blue:65/255, alpha: 1))
     }
     
-    public func textFieldDidEndEditing(_ textFieldEditingEnd: UITextField) {
+    public func updateTextAppearanceOnFieldDidEndEditing(_ textFieldEditingEnd: UITextField) {
         textFieldEditingEnd.layer.borderColor = UIColor.lightGray.cgColor
         textFieldEditingEnd.layer.borderWidth = 0
     }
