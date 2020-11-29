f77d9709cb97db333649787b45f438a910241429
bbalogovi <bbalogovi@foi.hr>
2020-11-29 13:20:49 +0100

    Created UI for registration completion screen and added avatar images

---
+++ client/WeatherActivity/Modules/Registration/RegistrationCompletionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationCompletionViewController.swift
+//  WeatherActivity
+//
+//  Created by Infinum on 28.11.2020..
+//
+
+import UIKit
+
+final class RegistrationCompletionViewController: UIViewController {
+
+    
+    @IBOutlet weak var avatarHorizontalStackView: UIStackView!
+//    override func viewDidAppear(_ animated: Bool) {
+//            super.viewDidAppear(animated)
+//            for _ in 0...10 {
+//                if let dayView = Bundle.main.loadNibNamed("DayView", owner: nil, options: nil)!.first as? DayView {
+//                    dayView.titleLabel.text = "Friday"
+//                    dayView.detailLabel.text = "A long detail text will be shown here"
+//                    avatarHorizontalStackView.addArrangedSubview(dayView)
+//                }
+//            }
+//        }
+}
