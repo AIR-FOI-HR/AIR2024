35830dc3e6a13b5a12f034285a8bb9b12f0887a7
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-01 12:53:00 +0100

    Edited req body parameters

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -4,19 +4,25 @@ var router = express.Router();
 var dbConnection = require('../database/connection')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
+const constants = require('../constants');
 
 router.post('/', function(req,res,next) {
     try {
         bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+            const token = jwt.sign({
+                email: req.body.email,
+                username: req.body.username
+            }, constants.jwtKey)
             let hashedPassword = hash    
-            let insertUserRegistrationSql = `INSERT INTO user VALUES ('${req.body.email}', 'xx123', '${hashedPassword}', '${req.body.name}', 'test', '123123', NULL);`
+            let insertUserRegistrationSql = `INSERT INTO user VALUES ('${req.body.email}', '${req.body.username}', '${hashedPassword}', '${req.body.firstName}', '${req.body.lastName}', '1', ${req.body.avatar}, '${token}');`
             console.log(insertUserRegistrationSql)
             dbConnection.connection.query(insertUserRegistrationSql, (err,data) => {
                 if(err) {
                     return res.json({"msg":"Error"})
                 } else {
-                    return res.json({"msg":"Inserted"})
-                    }
+                    return res.json({"msg":"Inserted", "token": token})
+                }
             })
         });
     } catch(err) {
