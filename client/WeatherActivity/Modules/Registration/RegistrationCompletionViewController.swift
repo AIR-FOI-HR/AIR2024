bda4b8413aaebc0e2af4feaacb4e19701c93d516
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-01 11:49:17 +0100

    Changed selecting avatar behaviour, moved registration service method to RegistrationCompletitionVC

--- client/WeatherActivity/Modules/Registration/RegistrationCompletionViewController.swift
+++ client/WeatherActivity/Modules/Registration/RegistrationCompletionViewController.swift
@@ -10,22 +10,47 @@ import UIKit
 final class RegistrationCompletionViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
-
-    var selectedAvatar = 1
-    @IBAction func avatarPresed(_ sender: UIButton) {
-        let currentAvatar = self.view.viewWithTag(sender.tag)
-        sender.selectedButtonAvatar(currentAvatar as! UIButton)
+    let alerter = Alerter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let previousAvatar = self.view.viewWithTag(selectedAvatar)
-        sender.deselectedButtonAvatar(previousAvatar as! UIButton)
-        
-        selectedAvatar = sender.tag
     }
-
+    
+    var selectedAvatar = 0
+    @IBAction func avatarPresed(_ sender: UIButton) {
+        if(selectedAvatar != 0){
+            let currentAvatar = self.view.viewWithTag(sender.tag)
+            sender.selectedButtonAvatar(currentAvatar as! UIButton)
+            
+            let previousAvatar = self.view.viewWithTag(selectedAvatar)
+            sender.deselectedButtonAvatar(previousAvatar as! UIButton)
+            
+            selectedAvatar = sender.tag
+        } else {
+            let currentAvatar = self.view.viewWithTag(sender.tag)
+            sender.selectedButtonAvatar(currentAvatar as! UIButton)
+            
+            selectedAvatar = sender.tag
+        }
+    }
+    
     @IBAction func finishButtonClicked(_ sender: UIButton) {
         guard let username = usernameTextField.text else { return }
-        let databaseAvatar = "avatar" + String(selectedAvatar)
-        print(databaseAvatar)
+        RegistrationUser.registrationUser.username = username
+        RegistrationUser.registrationUser.avatar = selectedAvatar
+        RegistrationService().register(userData: RegistrationUser.registrationUser, success: { registrationResponse in
+            debugPrint(registrationResponse)
+            if(registrationResponse.msg == "Error") {
+                return
+            }
+            self.performSegue(withIdentifier: "toHome", sender: self)
+        }, failure: {error in
+            debugPrint(error)
+            self.alerter.setAlerterData(title: "Oops!", message: "Error occured in registration process!")
+            self.present(self.alerter.alerter, animated: true, completion: nil)
+            return
+        })
     }
     
     @IBAction func registrationCompletionTextFieldDidBeginEditing(_ sender: UITextField) {
