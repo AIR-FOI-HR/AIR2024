9575bd7729ec374f315967c3b882fe8c28cafa20
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-01 20:48:57 +0100

    Created separated files for models, login models and registration models

---
+++ client/WeatherActivity/Common/Networking/Models/LoginModels/LoginCredentials.swift
@@ -0,0 +1,11 @@
+//
+//  LoginCredentials.swift
+//  WeatherActivity
+//
+//  Created by Kevin Bogdan on 01.12.2020..
+//
+
+struct LoginCredentials: Codable {
+    let email: String
+    let password: String
+}
