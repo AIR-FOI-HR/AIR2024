b133508c8bbffdfd022898199c73901870decd9f
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-01 13:11:54 +0100

    Changing default screen depending if user is logged

--- client/WeatherActivity/Application/SceneDelegate.swift
+++ client/WeatherActivity/Application/SceneDelegate.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import KeychainSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let keychain = KeychainSwift()
+    let loginService = LoginService()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        if let sessionToken = keychain.get("sessionToken") {
+            loginService.checkForToken(token: sessionToken, success: { checkResponse in
+                if(checkResponse.token == true){
+                    let homeViewController = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+                    self.window?.rootViewController = homeViewController
+                    self.window?.makeKeyAndVisible()
+                }
+            }, failure: { error in
+                
+            })
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
