20ed6ebdd394cb3e15b60dddb84dbf51944a8660
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-01 15:02:58 +0100

    Added enum for alert messages at registration

--- client/WeatherActivity/Modules/Registration/RegistrationViewController.swift
+++ client/WeatherActivity/Modules/Registration/RegistrationViewController.swift
@@ -14,37 +14,49 @@ final class RegistrationViewController: UIViewController {
     @IBOutlet weak private var passwordTextField: UITextField!
     @IBOutlet weak private var repeatPasswordTextField: UITextField!
     
+    enum AlertMessages: String {
+        case inputValuesError = "There was a problem with getting your input values"
+        case emptyFieldsError = "One or more fields are empty!"
+        case invalidEmailError = "You entered invalid e-mail format!"
+        case passwordMatchError = "Your passwords don't match!"
+        case passwordLengthError = "Password must be at least 6 characters long!"
+    }
+    
     @IBAction func registerButtonClick(_ sender: UIButton) {
         let alerter = Alerter()
-        alerter.addAction(title: "Ok")
+        let alertActionText = "Ok"
+        let alertTitle = "Oops!"
+        alerter.addAction(title: alertActionText)
+        
         guard let email = emailTextField.text, let password = passwordTextField.text , let firstName = firstNameTextField.text, let lastName = lastNameTextField.text,
         let repeatedPassword = repeatPasswordTextField.text else {
-            alerter.setAlerterData(title: "Oops", message: "There was a problem with getting your input values")
+            alerter.setAlerterData(title: alertTitle, message: AlertMessages.inputValuesError.rawValue)
             present(alerter.alerter, animated: true, completion: nil)
             return
         }
         
         let registrationValidator = RegistrationValidator(firstName: firstName, lastName: lastName, email: email, password: password, repeatedPassword: repeatedPassword)
         if(registrationValidator.emptyFieldExist()){
-            alerter.setAlerterData(title: "Error", message: "One or more fields are empty!")
+            alerter.setAlerterData(title: alertTitle, message: AlertMessages.emptyFieldsError.rawValue)
             present(alerter.alerter, animated: true, completion: nil)
             return
         }
         if(!registrationValidator.isValidEmail()){
-            alerter.setAlerterData(title: "Error", message: "You entered invalid e-mail format!")
+            alerter.setAlerterData(title: alertTitle, message: AlertMessages.invalidEmailError.rawValue)
             present(alerter.alerter, animated: true, completion: nil)
             return
         }
         if(!registrationValidator.isValidRepeatedPassword()){
-            alerter.setAlerterData(title: "Error", message: "Your passwords don't match!")
+            alerter.setAlerterData(title: alertTitle, message: AlertMessages.passwordMatchError.rawValue)
             present(alerter.alerter, animated: true, completion: nil)
             return
         }
         if(!registrationValidator.isValidPasswordLength()){
-            alerter.setAlerterData(title: "Error", message: "Password must be at least 6 characters long!")
+            alerter.setAlerterData(title: alertTitle, message: AlertMessages.passwordLengthError.rawValue)
             present(alerter.alerter, animated: true, completion: nil)
             return
         }
+        
         RegistrationUser.registrationUser.email = email
         RegistrationUser.registrationUser.password = password
         RegistrationUser.registrationUser.firstName = firstName
