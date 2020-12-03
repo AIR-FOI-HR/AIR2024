692280069b6116a56473e04aac31dfb596deb42b
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-03 20:34:22 +0100

    Added constants regard to date and time

--- client/WeatherActivity/Constants.swift
+++ client/WeatherActivity/Constants.swift
@@ -16,4 +16,12 @@ struct Constants {
     // MARK: User Defaults
     
     static let lastEnteredEmail = "LastEnteredEmail"
+    
+    // MARK: Date time
+    
+    static let defaultTime = "16:00"
+    static let defaultTimeInterval = 1
+    static let defaultDateFormat = "dd-MM-yyyy"
+    
+    // MARK: Openweather API
 }
