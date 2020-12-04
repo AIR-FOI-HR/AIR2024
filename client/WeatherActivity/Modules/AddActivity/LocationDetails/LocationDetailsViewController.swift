8873c27e892607aa50fb2e27cdbf8d5b3b4e9ea7
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-04 22:13:07 +0100

    Added base implementation of location suggestions on users input in text field

--- client/WeatherActivity/Modules/AddActivity/LocationDetails/LocationDetailsViewController.swift
+++ client/WeatherActivity/Modules/AddActivity/LocationDetails/LocationDetailsViewController.swift
@@ -10,19 +10,33 @@
 import UIKit
 import CoreLocation
 import MapKit
+import CHDropDownTextField
 
 class LocationDetailsViewController: UIViewController {
     
     // MARK: IBOutlets
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationTextField: CHDropDownTextField!
+    
     
     // MARK: Properties
     
     var locationManager = CLLocationManager()
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchSuggestions = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchCompleter.delegate = self
+        
+        locationTextField.addTarget(self, action: #selector(textFieldDidChangeValue), for: .editingChanged)
+        
+        locationTextField.dropDownTableVisibleRowCount = 3
+        locationTextField.dropDownDelegate = self
+        
         
         locationManager.delegate = self
         locationManager.requestLocation()
@@ -47,13 +61,14 @@ extension LocationDetailsViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
+        #warning("Handle error")
     }
 }
 
 // MARK: Map manipulation
 
 private extension LocationDetailsViewController {
+    
     func zoomMap(lat latitude: CLLocationDegrees, lon longitude: CLLocationDegrees,  setMapPoint setPoint: Bool) {
         let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -67,5 +82,42 @@ private extension LocationDetailsViewController {
         let mapPoint: MKPointAnnotation = MKPointAnnotation()
         mapPoint.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
         mapView.addAnnotation(mapPoint)
+    }
+}
+
+// MARK: Search completer
+
+extension LocationDetailsViewController: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        for res in completer.results {
+            self.searchSuggestions.append(res.title)
+        }
+        self.locationTextField.dropDownTableTitlesArray = self.searchSuggestions
+    }
+    
+    private func completer(completer: MKLocalSearchCompleter, didFailWithError error: NSError) {
+        print("NOPE")
+        #warning("Handle error")
+    }
+}
+
+// MARK: Text field manager
+
+extension LocationDetailsViewController {
+    
+    @objc func textFieldDidChangeValue() {
+        if(locationTextField.text!.count > 3) {
+            searchCompleter.queryFragment = locationTextField.text!
+        }
+    }
+}
+
+// MARK: CHDropDown delegate
+
+extension LocationDetailsViewController: CHDropDownTextFieldDelegate {
+    
+    func dropDownTextField(_ dropDownTextField: CHDropDownTextField!, didChooseDropDownOptionAt index: UInt) {
+        self.locationTextField.text = self.searchSuggestions[Int(index)]
     }
 }
