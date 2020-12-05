72d918026b16ac34ba44350287b5b126d194edb2
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-05 14:26:21 +0100

    Added WeatherData struct for OpenWeatherMap API

--- client/WeatherActivity/Common/Networking/Models/WeatherModels/WeatherData.swift
+++ client/WeatherActivity/Common/Networking/Models/WeatherModels/WeatherData.swift
@@ -5,15 +5,70 @@
 //  Created by Kevin Bogdan on 04.12.2020..
 //
 
+// MARK: - Welcome
+
 struct WeatherData: Codable {
-    let name: String
-    let main: Main
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let timezone, id: Int?
+    let name: String?
+    let cod: Int?
 }
 
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int?
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double?
+}
+
+// MARK: - Main
 struct Main: Codable {
-    let temp: Double
-    let temp_min: Double
-    let temp_max: Double
-    let humidity: Double
+    let temp, feelsLike, tempMin, tempMax: Double?
+    let pressure, humidity: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise, sunset: Int?
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int?
+    let main, weatherDescription, icon: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double?
+    let deg: Int?
 }
 
