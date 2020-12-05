a6d4d649e23aeecf96de391236c0934543856d3f
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-05 14:46:09 +0100

    Edited WeatherData class, removed unneeded propertie

--- client/WeatherActivity/Common/Networking/Models/WeatherModels/WeatherData.swift
+++ client/WeatherActivity/Common/Networking/Models/WeatherModels/WeatherData.swift
@@ -8,18 +8,43 @@
 // MARK: - Welcome
 
 struct WeatherData: Codable {
-    let coord: Coord?
-    let weather: [Weather]?
-    let base: String?
-    let main: Main?
-    let visibility: Int?
-    let wind: Wind?
-    let clouds: Clouds?
-    let dt: Int?
-    let sys: Sys?
-    let timezone, id: Int?
+    let cod: String?
+    let message, cnt: Int?
+    let list: [List]?
+    let city: City?
+}
+
+// MARK: - City
+struct City: Codable {
+    let id: Int?
     let name: String?
-    let cod: Int?
+    let coord: Coord?
+    let country: String?
+    let population, timezone, sunrise, sunset: Int?
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lat, lon: Double?
+}
+
+// MARK: - List
+struct List: Codable {
+    let dt: Int?
+    let main: MainClass?
+    let weather: [Weather]?
+    let clouds: Clouds?
+    let wind: Wind?
+    let visibility: Int?
+    let pop: Double?
+    let dtTxt: String?
+    let rain: Rain?
+
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, visibility, pop
+        case dtTxt = "dt_txt"
+        case rain
+    }
 }
 
 // MARK: - Clouds
@@ -27,38 +52,41 @@ struct Clouds: Codable {
     let all: Int?
 }
 
-// MARK: - Coord
-struct Coord: Codable {
-    let lon, lat: Double?
-}
-
-// MARK: - Main
-struct Main: Codable {
+// MARK: - MainClass
+struct MainClass: Codable {
     let temp, feelsLike, tempMin, tempMax: Double?
-    let pressure, humidity: Int?
-    
+    let pressure, seaLevel, grndLevel, humidity: Int?
+    let tempKf: Double?
+
     enum CodingKeys: String, CodingKey {
         case temp
         case feelsLike = "feels_like"
         case tempMin = "temp_min"
         case tempMax = "temp_max"
-        case pressure, humidity
+        case pressure
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
     }
 }
 
-// MARK: - Sys
-struct Sys: Codable {
-    let type, id: Int?
-    let message: Double?
-    let country: String?
-    let sunrise, sunset: Int?
+// MARK: - Rain
+struct Rain: Codable {
+    let the3H: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case the3H = "3h"
+    }
 }
 
 // MARK: - Weather
 struct Weather: Codable {
     let id: Int?
-    let main, weatherDescription, icon: String?
-    
+    let main: String?
+    let weatherDescription: String?
+    let icon: String?
+
     enum CodingKeys: String, CodingKey {
         case id, main
         case weatherDescription = "description"
@@ -71,4 +99,3 @@ struct Wind: Codable {
     let speed: Double?
     let deg: Int?
 }
-
