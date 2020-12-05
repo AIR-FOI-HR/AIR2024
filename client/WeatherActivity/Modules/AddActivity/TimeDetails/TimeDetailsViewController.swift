08544174628616f7a2b829fad0f9a72e1eebd9cc
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-05 16:40:00 +0100

    Added presentData function to present data to UI, added function isDateRangeValid for validation of 5 days range interval, added Function for combining picker time with picked date

--- client/WeatherActivity/Modules/AddActivity/TimeDetails/TimeDetailsViewController.swift
+++ client/WeatherActivity/Modules/AddActivity/TimeDetails/TimeDetailsViewController.swift
@@ -33,7 +33,7 @@ class TimeDetailsViewController: UIViewController {
     let dateFormatter = DateFormatter()
     var locationDetails: LocationDetails?
     let weatherManager = WeatherManager()
-    #warning("Delete dummy location")
+    #warning("Delete dummy location and replace with previous screen data")
     let dummyLocation = LocationDetails(latitude: 46.306268, longitude: 16.336089)
     
     override func viewDidLoad() {
@@ -59,11 +59,11 @@ private extension TimeDetailsViewController {
     
     func getForecast(date: Date) {
         weatherManager.getWeatherForecast(date: date, locationCoordinates: dummyLocation) { weatherData in
-            self.presentData(weatherData: weatherData)
+            self.getForecastForDate(forDate: date, weatherData: weatherData)
         } failure: { error in
+            print(error)
             #warning("Handle error")
         }
-
     }
 }
 
@@ -71,13 +71,22 @@ private extension TimeDetailsViewController {
 
 private extension TimeDetailsViewController {
     
-    func presentData(weatherData: WeatherData) {
-        
-        guard let temperature = weatherData.main?.temp, let feelsLikeTemp = weatherData.main?.feelsLike, let windSpeed = weatherData.wind?.speed, let humidity = weatherData.main?.humidity else { return }
-        self.temperatureLabel.text = String(temperature)
-        self.temperatureFeelsLikeLabel.text = String(feelsLikeTemp)
-        self.windLabel.text = String(format: "%.2f", windSpeed * 3.6)
-        self.humidityLabel.text = String(humidity)
+    func presentData(weatherData: WeatherList) {
+        guard let temperature = weatherData.main?.temp, let feelsLike = weatherData.main?.feelsLike, let windSpeed = weatherData.wind?.speed, let humidity = weatherData.main?.humidity else { return }
+        temperatureLabel.text = String(temperature)
+        temperatureFeelsLikeLabel.text = String(feelsLike)
+        windLabel.text = String(windSpeed)
+        humidityLabel.text = String(humidity)
+    }
+    
+    func getForecastForDate(forDate date: Date, weatherData data: WeatherData) {
+        guard let dataList = data.weatherList else { return }
+        for index in 1...dataList.count - 2 {
+            let range = Date(timeIntervalSince1970: dataList[index].dt!)...Date(timeIntervalSince1970: dataList[index+1].dt!)
+            if range.contains(date) {
+                self.presentData(weatherData: dataList[index])
+            }
+        }
     }
 }
 
@@ -87,12 +96,16 @@ private extension TimeDetailsViewController {
     
     @objc func datePickerEndEditing() {
         dateFormatter.dateFormat = Constants.defaultDateFormat
-        getForecast(date: datePicker.date)
+        #warning("Handle if date is not in range")
+        isDateRangeValid(date: datePicker.date) ? getForecast(date: datePicker.date) : print("Not in range")
     }
     
     @objc func fromTimePickerEndEditing() {
         let suggestedUntilTime = addTime(to: fromTimePicker.date)
         untilTimePicker.date = suggestedUntilTime
+        let newDate = combineDateAndTime(date: datePicker.date, time: fromTimePicker.date)
+        #warning("Handle if date is not in range")
+        isDateRangeValid(date: newDate) ? getForecast(date: newDate) : print("Not in range")
     }
     
     func setDefault() {
@@ -110,5 +123,34 @@ private extension TimeDetailsViewController {
     func addTime(to toDate: Date, hours hoursValue: Int = Constants.defaultTimeInterval) -> Date {
         guard let modifiedDate = Calendar.current.date(byAdding: .hour, value: hoursValue, to: toDate) else { return toDate }
         return modifiedDate
+    }
+    
+    // Maximum of 5 days apart -> Reason: OpenWeatherMap API Free version gives only forecast for 5 days infront
+    func isDateRangeValid(date: Date) -> Bool {
+        let nowDate = Date()
+        let calendar = Calendar.current
+        let nextDate = calendar.date(byAdding: .day, value: Constants.validDateRange, to: nowDate)
+        
+        let range = nowDate...nextDate!
+        if range.contains(date) {
+            return true
+        }
+        return false
+    }
+    
+    func combineDateAndTime(date: Date, time: Date) -> Date {
+        let calendar = Calendar.current
+        
+        let timeComponents: DateComponents? = calendar.dateComponents([.hour, .minute, .second], from: time)
+        var dateComponents: DateComponents? = calendar.dateComponents([.day, .month, .year], from: date)
+        
+        dateComponents?.hour = timeComponents?.hour
+        dateComponents?.minute = timeComponents?.minute
+        dateComponents?.second = timeComponents?.second
+        
+        if let newDate: Date = calendar.date(from: dateComponents!) {
+            return newDate
+        }
+        return date
     }
 }
