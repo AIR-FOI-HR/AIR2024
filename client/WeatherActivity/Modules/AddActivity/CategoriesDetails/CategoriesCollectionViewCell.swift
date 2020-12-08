c6666b528e77be94254e924b5365ecd79cf6045c
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-08 14:10:44 +0100

    Added CategoriesCollectionViewCell

---
+++ client/WeatherActivity/Modules/AddActivity/CategoriesDetails/CategoriesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  WeatherActivity
+//
+//  Created by Infinum on 07.12.2020..
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var categoryName: UILabel!
+    
+    // TODO: add categoryImage outlet
+    
+    
+    // TODO: add handler for selecting cell
+}
