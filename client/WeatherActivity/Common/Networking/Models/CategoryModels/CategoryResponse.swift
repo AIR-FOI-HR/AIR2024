d88b30bfdbc3baabc7776622192989e5714129e3
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-08 14:11:23 +0100

    Added category service and response

---
+++ client/WeatherActivity/Common/Networking/Models/CategoryModels/CategoryResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryResponse.swift
+//  WeatherActivity
+//
+//  Created by Infinum on 06.12.2020..
+//
+
+import Foundation
+
+struct CategoryResponse: Decodable {
+    let categories: [String]
+}
