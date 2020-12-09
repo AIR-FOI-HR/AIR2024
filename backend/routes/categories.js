7c28094a415cb514a5eb0a9d981f90eb3bdc7fd6
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-09 14:34:13 +0100

    Added query for getting recent categories from database

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -6,19 +6,45 @@ router.get('/allCategories', function(req, res, next) {
     try{
         dbConnection.connection.query(`select * from category order by name asc`, (err, data) => {
             if (err) {
-                return res.json({ "categories": "false" })
+                return res.json({ "empty": true })
             }
             else if (data.length > 0) {
                 var allCategoryNames = data.map(x => x.name);
-                return res.json({"categories": allCategoryNames})
+                return res.json({"categories": allCategoryNames, "empty": false})
             }
             else {
-                return res.json({ "categories": "false" })
+                return res.json({ "empty": true})
             }
         })
     }
     catch(err){
-        return res.json({"categories": "false"})
+        return res.json({"empty": true})
+    }
+});
+
+router.post('/recentCategories', function(req, res, next) {
+    try{
+        let recentCategoriesQuery = `SELECT DISTINCT(category.name) FROM activity
+        join category on (activity.categoryId = category.categoryId)
+        join doing on (doing.activityId = activity.activityId)
+        join user on (doing.mail = user.mail)
+        where user.mail = '${req.body.email}'
+        order by created_at desc limit 4`;
+        dbConnection.connection.query(recentCategoriesQuery, (err, data) => {
+            if (err) {
+                return res.json({ "empty": true })
+            }
+            else if (data.length > 0) {
+                var allCategoryNames = data.map(x => x.name);
+                return res.json({"categories": allCategoryNames, "empty": false})
+            }
+            else {
+                return res.json({"empty": true})
+            }
+        })
+    }
+    catch(err){
+        return res.json({"empty": true})
     }
 });
 
