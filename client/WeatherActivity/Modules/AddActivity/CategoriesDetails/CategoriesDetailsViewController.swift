5c62165e1e9c04c3383f053b71eabc633e0b98f9
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-09 14:31:30 +0100

    Added recent categories in storyboard and VC

--- client/WeatherActivity/Modules/AddActivity/CategoriesDetails/CategoriesDetailsViewController.swift
+++ client/WeatherActivity/Modules/AddActivity/CategoriesDetails/CategoriesDetailsViewController.swift
@@ -11,7 +11,7 @@ final class CategoryDetailsViewController: UIViewController, UICollectionViewDat
     
     // MARK: IBOutlets
 
-    @IBOutlet weak var horizontalStack: UIStackView!
+    @IBOutlet weak var horizontalStackView: UIStackView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
         
@@ -19,20 +19,80 @@ final class CategoryDetailsViewController: UIViewController, UICollectionViewDat
     
     let categoryService = CategoryService()
     var allCategories = [String]()
+    var recentCategories = [String]()
     var numberOfPages: Int = 0
-
+    var emptyResponse: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
         getAllCategories()
         setCollectionViewLayout()
+        setRecentCategories()
+    }
+    
+    func updateHorizontalStackView() {
+        // TODO: add images for each category
+            
+        horizontalStackView.axis  = NSLayoutConstraint.Axis.horizontal
+        horizontalStackView.distribution  = UIStackView.Distribution.fillEqually
+        horizontalStackView.alignment = UIStackView.Alignment.center
+        horizontalStackView.spacing   = 10.0
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if !emptyResponse {
+            createLabelsForRecentCategories()
+        }
+        else {
+            createLabelForNoRecentCategories()
+        }
+    }
+    
+    func createLabelsForRecentCategories() {
+        for category in recentCategories {
+            let textLabel = UILabel()
+            textLabel.textAlignment = .center
+            textLabel.textColor = UIColor.systemGray
+            textLabel.font = textLabel.font.withSize(13)
+            textLabel.text  = category
+            horizontalStackView.addArrangedSubview(textLabel)
+        }
+    }
+    
+    func createLabelForNoRecentCategories() {
+        let textLabel = UILabel()
+        textLabel.textAlignment = .center
+        textLabel.textColor = UIColor.systemGray
+        textLabel.font = textLabel.font.withSize(16)
+        textLabel.text  = "You don't have any recent categories"
+        horizontalStackView.addArrangedSubview(textLabel)
+    }
+    
+    func setRecentCategories() {
+        let email = "test3@gmail.com"
+        // TODO: get logged user's mail (not hardcoded)
+        categoryService.getRecentCategories(userEmail: email, success: { apiResponse in
+            if(apiResponse.empty) {
+                self.emptyResponse = true
+            }
+            else {
+                self.recentCategories = apiResponse.categories!
+                self.emptyResponse = false
+            }
+            
+            self.updateHorizontalStackView()
+        }, failure: { error in
+            print(error.localizedDescription)
+            self.presentAlert(title: "Oops!", message: "Something went wrong with getting your recent categories!")
+        })
     }
     
     func getAllCategories() {
         categoryService.getAllCategories(success: { apiResponse in
-            self.allCategories = apiResponse.categories
-            self.numberOfPages = Int(self.allCategories.count / 6) + 1 // TODO
+            self.allCategories = apiResponse.categories!
+            self.numberOfPages = Int(self.allCategories.count / 6)
+            // TODO: fix bug with page control not recognizing last page and not setting propiate number of pages
             self.updateCollectionView()
         }, failure: { error in
             print(error.localizedDescription)
@@ -64,9 +124,9 @@ final class CategoryDetailsViewController: UIViewController, UICollectionViewDat
     }
     
     func updateCollectionView() {
-        DispatchQueue.main.async {
-            self.collectionView.reloadData()
-        }
+//        DispatchQueue.main.async {
+        self.collectionView.reloadData()
+//        }
     }
     
     func setCollectionViewLayout() {
@@ -80,7 +140,6 @@ final class CategoryDetailsViewController: UIViewController, UICollectionViewDat
         layout.minimumInteritemSpacing = 0
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView!.collectionViewLayout = layout
-
     }
     
 }
