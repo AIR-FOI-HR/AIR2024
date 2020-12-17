5854d22557fd236c6d84791d4135835d1d64ba7b
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-17 11:47:16 +0100

    Added class and protocol used in every view controller

---
+++ client/WeatherActivity/Modules/AddActivity/Base/AddActivityStepViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddActivityStepViewController.swift
+//  WeatherActivity
+//
+//  Created by Kevin Bogdan on 17.12.2020..
+//
+
+import UIKit
+
+// MARK: - Protocol
+
+protocol SetupButtons {
+    func hideNextButton()
+    func hidePreviousButton()
+}
+
+class AddActivityStepViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var flowNavigator: AddActivityFlowNavigator?
+    var setupDelegate: SetupButtons?
+    
+    // MARK: - Methods
+    
+    func setupButtons(step: StepInfo) {
+        
+        guard let flowNavigator = flowNavigator else { return }
+        
+        if flowNavigator.isLastStep(step: step) {
+            setupDelegate?.hideNextButton()
+        }
+        
+        if flowNavigator.isFirstStep(step: step) {
+            setupDelegate?.hidePreviousButton()
+        }
+        
+    }
+}
