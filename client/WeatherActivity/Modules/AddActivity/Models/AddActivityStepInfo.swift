baea6766d64041256e93c6526f63516aa53e600d
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-17 11:49:13 +0100

    StepInfo enum

---
+++ client/WeatherActivity/Modules/AddActivity/Models/AddActivityStepInfo.swift
@@ -0,0 +1,15 @@
+//
+//  AddActivityStepInfo.swift
+//  WeatherActivity
+//
+//  Created by Kevin Bogdan on 17.12.2020..
+//
+
+import UIKit
+
+enum StepInfo: String {
+    case locationDetails = "LocationDetails"
+    case timeDetails = "TimeDetails"
+    case categoryDetails = "CategoryDetails"
+    case finalDetails = "FinalDetails"
+}
