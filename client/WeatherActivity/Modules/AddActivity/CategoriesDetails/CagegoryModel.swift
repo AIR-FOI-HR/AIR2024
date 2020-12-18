20be8968a27149901f23eee325e9aedafb12d0f1
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-18 17:01:26 +0100

    Added Category enum

---
+++ client/WeatherActivity/Modules/AddActivity/CategoriesDetails/CagegoryModel.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryDetailsModel.swift
+//  WeatherActivity
+//
+//  Created by Infinum on 18.12.2020..
+//
+
+import Foundation
+
+class Categories {
+    
+    public enum AllCategories: String, CaseIterable {
+        case sports = "Sports"
+        case pets = "Pets"
+        case tv = "TV"
+        case videogames = "Videogames"
+        case sleeping = "Sleeping"
+        case cinema = "Cinema"
+        case cooking = "Cooking"
+        case eating = "Eating"
+        case fishing = "Fishing"
+        case farming = "Farming"
+        case gardening = "Gardening"
+        case programming = "Programming"
+        case socializing = "Socializing"
+        case walking = "Walking"
+        case running = "Running"
+        case climbing = "Climbing"
+        case woodcutting = "Woodcutting"
+    //    case unknow = "unknowCategory"
+    }
+    
+}
+
