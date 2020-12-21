bd0c43ce5721cf729dafcea56dcb99fbfdfea65a
Patrik Črnčec <rnecpatrik95@gmail.com>
2020-12-21 15:00:35 +0100

    Backend for categories changed

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var dbConnection = require('../database/connection')
+var dbConnection = require('../database/connection');
 
 router.get('/allCategories', function(req, res, next) {
     try{
         dbConnection.connection.query(`select * from category order by name asc`, (err, data) => {
             if (err) {
-                return res.json({ "empty": true })
+                return res.json({ "categories": [] });
             }
             else if (data.length > 0) {
                 var allCategoryNames = data.map(x => x.name);
-                return res.json({"categories": allCategoryNames, "empty": false})
+                jsonObject = {};
+                jsonObject.categories = [];
+                allCategoryNames.forEach(category => {
+                    jsonObject.categories.push({"categoryName": category});    
+                });
+                return res.json(jsonObject)
             }
             else {
-                return res.json({ "empty": true})
+                return res.json({ "categories": []});
             }
         })
     }
     catch(err){
-        return res.json({"empty": true})
+        return res.json({"categories": []});
     }
 });
 
@@ -28,23 +33,28 @@ router.post('/recentCategories', function(req, res, next) {
         join category on (activity.categoryId = category.categoryId)
         join doing on (doing.activityId = activity.activityId)
         join user on (doing.mail = user.mail)
-        where user.mail = '${req.body.email}'
+        where user.sessionToken = '${req.body.sessionToken}'
         order by created_at desc limit 4`;
         dbConnection.connection.query(recentCategoriesQuery, (err, data) => {
             if (err) {
-                return res.json({ "empty": true })
+                return res.json({ "categories": [] });
             }
             else if (data.length > 0) {
                 var allCategoryNames = data.map(x => x.name);
-                return res.json({"categories": allCategoryNames, "empty": false})
+                jsonObject = {};
+                jsonObject.categories = [];
+                allCategoryNames.forEach(category => {
+                    jsonObject.categories.push({"categoryName": category});    
+                });
+                return res.json(jsonObject);
             }
             else {
-                return res.json({"empty": true})
+                return res.json({"categories": []});
             }
         })
     }
     catch(err){
-        return res.json({"empty": true})
+        return res.json({"categories": []});
     }
 });
 
