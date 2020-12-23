7d04496502a6f56760c2023e4a20cfaab719fa19
Kevin Bogdan <kevinwebd31@gmail.com>
2020-12-23 12:45:40 +0100

    Added method for getting data from step

--- client/WeatherActivity/Modules/AddActivity/Utilities/AddActivityFlowDataManager.swift
+++ client/WeatherActivity/Modules/AddActivity/Utilities/AddActivityFlowDataManager.swift
@@ -9,25 +9,40 @@ import Foundation
 
 class AddActivityFlowDataManager {
     
+    // MARK: - Properties
+    
     var stepsData: [StepData] = []
+    
+    // MARK: - Methods
     
     func saveData(data: StepData) {
         
+        stepsData.removeAll(where: { $0.stepInfo == data.stepInfo })
         stepsData.append(data)
-        dataToJson()
+        print(stepsData)
     }
     
-    func dataToJson() {
-        #warning("Preparing, ignore")
+    func dataToJson() -> String? {
+        
         var dictionary = [String:Any]()
         stepsData.forEach { (stepData) in
             if let dictionaryInfo = try? stepData.data.asDictionary() {
                 dictionary[stepData.stepInfo.rawValue] = dictionaryInfo
             }
         }
-        let data = try? JSONSerialization.data(withJSONObject: dictionary, options: .fragmentsAllowed)
-        let json = String(data: data!, encoding: .ascii)
-        print("Dictionary: \(dictionary)")
-        print("Json: \(json)")
+        #warning("Handle if failed to parse to JSON")
+        guard let data = try? JSONSerialization.data(withJSONObject: dictionary, options: .fragmentsAllowed) else { return nil }
+        let json = String(data: data, encoding: .ascii)
+        return json
+    }
+    
+    func getData<T>(forStep step: StepInfo) -> T? {
+        
+        guard let data = stepsData
+                .first(where: { $0.stepInfo == step })
+                .map({ $0.data })
+        else { return nil }
+        return data as? T
+        
     }
 }
