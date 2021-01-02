5acc4deb1e806d421df463bcfbde50326b594235
bbalogovi <bbalogovi@foi.hr>
2021-01-02 21:08:14 +0100

    Added auto layout and collection view

--- client/WeatherActivity/Modules/Registration/RegistrationCompletionViewController.swift
+++ client/WeatherActivity/Modules/Registration/RegistrationCompletionViewController.swift
@@ -11,11 +11,36 @@ enum RegistrationCompletionNavigation: String {
     case home = "CompletionToHome"
 }
 
-final class RegistrationCompletionViewController: UIViewController {
+enum Avatars: String, CaseIterable {
+    case avatar1 = "avatar1"
+    case avatar2 = "avatar2"
+    case avatar3 = "avatar3"
+    case avatar4 = "avatar4"
+    case avatar5 = "avatar5"
+    case avatar6 = "avatar6"
+    case avatar7 = "avatar7"
+    case avatar8 = "avatar8"
+    case avatar9 = "avatar9"
+    case avatar10 = "avatar10"
+}
+
+//MARK: - AvatarCell class
+
+class AvatarCell {
+    var avatar: Avatars
+    var image: UIImage? { UIImage(named: avatar.rawValue) }
+    
+    init(imageName: Avatars) {
+        self.avatar = imageName
+    }
+}
+
+final class RegistrationCompletionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     // MARK: IBOutlets
     
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: Properties
     
@@ -25,26 +50,87 @@ final class RegistrationCompletionViewController: UIViewController {
     var userPreferences: UserPreferences?
     var selectedAvatar = 0
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
     // MARK: IBActions
     
-    @IBAction func avatarPresed(_ sender: UIButton) {
-        if(selectedAvatar != 0){
-            let currentAvatar = self.view.viewWithTag(sender.tag)
-            sender.selectedButtonAvatar(currentAvatar as! UIButton)
-            
-            let previousAvatar = self.view.viewWithTag(selectedAvatar)
-            sender.deselectedButtonAvatar(previousAvatar as! UIButton)
-            
-            selectedAvatar = sender.tag
+    //    @IBAction func avatarPresed(_ sender: UIButton) {
+    //        if(selectedAvatar != 0){
+    //            let currentAvatar = self.view.viewWithTag(sender.tag)
+    //            sender.selectedButtonAvatar(currentAvatar as! UIButton)
+    //
+    //            let previousAvatar = self.view.viewWithTag(selectedAvatar)
+    //            sender.deselectedButtonAvatar(previousAvatar as! UIButton)
+    //
+    //            selectedAvatar = sender.tag
+    //        } else {
+    //            let currentAvatar = self.view.viewWithTag(sender.tag)
+    //            sender.selectedButtonAvatar(currentAvatar as! UIButton)
+    //
+    //            selectedAvatar = Int(sender.tag)
+    //        }
+    //    }
+    
+    //MARK: - CollectionVIew handling
+    
+    var avatarNames: [AvatarCell] {
+        [.init(imageName: .avatar1), .init(imageName: .avatar2), .init(imageName: .avatar3), .init(imageName: .avatar4), .init(imageName: .avatar5), .init(imageName: .avatar6), .init(imageName: .avatar7), .init(imageName: .avatar8), .init(imageName: .avatar9), .init(imageName: .avatar10)]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Avatars.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? AvatarCollectionViewCell else { fatalError() }
+        
+        var avatarImages = [UIImage]()
+        for name in avatarNames {
+            if let avatarImage = name.image {
+                avatarImages.append(avatarImage)
+            }
+        }
+        cell.avatarImageView.image = avatarImages[indexPath.item]
+        
+        return cell
+    }
+    
+    //MARK: - CollectionVIew: SelectedItems
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        if selectedAvatar == indexPath.item + 1 {
+            cell.layer.borderWidth = 0
+            selectedAvatar = 0
         } else {
-            let currentAvatar = self.view.viewWithTag(sender.tag)
-            sender.selectedButtonAvatar(currentAvatar as! UIButton)
-            
-            selectedAvatar = Int(sender.tag)
+            cell.isSelected = true
+            cell.layer.borderWidth = 1.0
+            cell.layer.borderColor = UIColor(red:115/255, green:204/255, blue:255/255, alpha: 1).cgColor
+            selectedAvatar = indexPath.item + 1
         }
     }
     
+    //MARK: - CollectionVIew: DeselectedItems
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        cell.isSelected = false
+        selectedAvatar = 0
+        cell.layer.borderWidth = 0
+    }
+    
+    //MARK: - IBActions
+    
     @IBAction func finishButtonClicked(_ sender: UIButton) {
+        if selectedAvatar == 0 {
+            selectedAvatar = 1
+        }
         guard let username = usernameTextField.text else { return }
         self.userPreferences = UserPreferences(username: username, avatarId: selectedAvatar)
         let registrationData = RegistrationData(first: userInformation, second: userPreferences)
