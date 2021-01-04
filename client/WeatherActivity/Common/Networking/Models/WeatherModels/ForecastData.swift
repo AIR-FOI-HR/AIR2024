a6a0f704dcb74d227503240f816ad9863868a45d
Kevin Bogdan <kevinwebd31@gmail.com>
2021-01-04 14:10:26 +0100

    added function for getting condition id

--- client/WeatherActivity/Common/Networking/Models/WeatherModels/ForecastData.swift
+++ client/WeatherActivity/Common/Networking/Models/WeatherModels/ForecastData.swift
@@ -28,4 +28,25 @@ struct ForecastData {
         }
         return image
     }
+    
+    func getConditionId(id: Int) -> Int {
+        switch id {
+        case 800:
+            return 1
+        case 803...804:
+            return 2
+        case 802:
+            return 3
+        case 300...531:
+            return 4
+        case 701...741:
+            return 5
+        case 600-622:
+            return 6
+        case 200...232:
+            return 7
+        default:
+            return 8
+        }
+    }
 }
