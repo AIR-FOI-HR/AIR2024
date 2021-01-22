fe0b07d27cfaad42acdfdfbcdb0f597c1f836a5d
KevinBdev <bogdan.kevin016@gmail.com>
2021-01-22 11:02:24 +0100

    added class and changed names of the existing ones

--- client/WeatherActivity/Common/Views/ActivityListView/Cell/ActivityCell.swift
+++ client/WeatherActivity/Common/Views/ActivityListView/Cell/ActivityCell.swift
@@ -26,7 +26,7 @@ protocol ActivityCellItemP {
     var statusType: String { get }
 }
 
-class CustomActivityCellItem: ActivityCellItemP {
+class DefaultActivityCellItem: ActivityCellItemP {
     var activityId: Int
     var startTime: String
     var endTime: String
@@ -82,7 +82,7 @@ class CustomActivityCellItem: ActivityCellItemP {
     }
 }
 
-class MyCustomActivityCellItem: ActivityCellItemP {
+class InProgressActivityCellItem: ActivityCellItemP {
     var activityId: Int
     var startTime: String
     var endTime: String
@@ -122,7 +122,63 @@ class MyCustomActivityCellItem: ActivityCellItemP {
         self.activityId = activityId
         self.startTime = startTime
         self.endTime = endTime
-        self.title = "Mod tit: " + title
+        self.title = "IP:" + title
+        self.description = description
+        self.locationName = locationName
+        self.latitude = latitude
+        self.longitude = longitude
+        self.temperature = temperature
+        self.feelsLike = feelsLike
+        self.wind = wind
+        self.humidity = humidity
+        self.forecastType = forecastType
+        self.name = name
+        self.type = type
+        self.statusType = statusType
+    }
+}
+
+class FinishedActivityCellItem: ActivityCellItemP {
+    var activityId: Int
+    var startTime: String
+    var endTime: String
+    var title: String
+    var description: String
+    var locationName: String
+    var latitude: Double
+    var longitude: Double
+    var temperature: Float?
+    var feelsLike: Float?
+    var wind: Float?
+    var humidity: Int?
+    var forecastType: String?
+    var name: String
+    var type: String
+    var statusType: String
+    
+    init(
+        activityId: Int,
+        startTime: String,
+        endTime: String,
+        title: String,
+        description: String,
+        locationName: String,
+        latitude: Double,
+        longitude: Double,
+        temperature: Float?,
+        feelsLike: Float?,
+        wind: Float?,
+        humidity: Int?,
+        forecastType: String?,
+        name: String,
+        type: String,
+        statusType: String
+    ) {
+        
+        self.activityId = activityId
+        self.startTime = startTime
+        self.endTime = endTime
+        self.title = "FF:" + title
         self.description = description
         self.locationName = locationName
         self.latitude = latitude
